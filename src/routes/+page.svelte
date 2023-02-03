96c0dd57034ce5a616874a88618b89c09f7047b8
Jordan Calhoun <jordancalhoun@gmail.com>
2023-02-03 11:36:01 -0500

    MVP

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,53 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+	<title>Code Carton</title>
+</svelte:head>
+<header>
+	<img src="/code-carton-logo.svg" alt="Code Carton Logo" />
+	<nav>
+		<a href="https://twitter.com/codecarton" rel="norefferr">Twitter</a>
+		<a href="mailto:hello@codecarton.com">Email</a>
+	</nav>
+</header>
+
+<div>
+	<img src="/iphone-screenshot.png" alt="Screnshot of Fractions iOS App on an iPhone 14 Pro" />
+	<br />
+	<img src="/Fractions.svg" alt="The word Fractions" />
+	<p style="color: white; font-family: sans-serif;">
+		A fractional calculator app designed for woodworkers and other makers.
+	</p>
+	<a href="fractionsapp.com" style="color: #D447F0;">Sign up for beta waiting list</a>
+</div>
+
+<style>
+	:global(body) {
+		background-color: #1f1f1f;
+		padding: 20px;
+		text-align: center;
+		display: block;
+		margin: 0 auto;
+	}
+
+	a {
+		color: white;
+		font-family: Arial, Helvetica, sans-serif;
+		text-decoration: none;
+	}
+
+	a:hover {
+		font-weight: 600;
+	}
+
+	nav {
+		margin-top: 10px;
+		margin-bottom: 10px;
+	}
+
+	nav a {
+		margin: 0 15px;
+	}
+
+	div img {
+		margin-top: 10px;
+	}
+</style>
