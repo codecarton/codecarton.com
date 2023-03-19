db5f338068f1bbb15890427fff424a0962e68a61
Jordan Calhoun <jordancalhoun@gmail.com>
2023-03-19 14:53:19 -0400

    updates for app store review

---
+++ src/routes/one16/+page.svelte
@@ -0,0 +1,72 @@
+<svelte:head>
+	<title>One16 by Code Carton</title>
+</svelte:head>
+<header>
+	<img src="/code-carton-logo.svg" alt="Code Carton Logo" />
+	<nav>
+		<a href="https://twitter.com/codecarton" rel="norefferr">Twitter</a>
+		<a href="mailto:hello@codecarton.com">Email</a>
+	</nav>
+</header>
+
+<div>
+	<img src="/iPhone.png" alt="Screnshot of Fractions iOS App on an iPhone 14 Pro" width="300px" />
+	<br />
+	<h2>One16</h2>
+	<p style="color: white; font-family: sans-serif;">
+		A fractional calculator designed for woodworkers and other makers.
+	</p>
+	<!-- <a href="https://fractionsapp.com" style="color: #D447F0;">Sign up for beta waiting list</a> -->
+
+	<a href="one16/privacy" style="color: #777;margin-top: 100px;display: block;" target="_blank"
+		>Privacy Policy</a
+	>
+</div>
+
+<style>
+	:global(body) {
+		background-color: #1f1f1f;
+		padding: 20px;
+		text-align: center;
+		display: block;
+		margin: 0 auto;
+	}
+
+	h2 {
+		font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu,
+			Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+		font-weight: 600;
+		color: white;
+		font-size: 60px;
+	}
+
+	p {
+		font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu,
+			Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+		color: white;
+		font-size: 20px;
+	}
+
+	a {
+		color: white;
+		font-family: Arial, Helvetica, sans-serif;
+		text-decoration: none;
+	}
+
+	a:hover {
+		font-weight: 600;
+	}
+
+	nav {
+		margin-top: 10px;
+		margin-bottom: 10px;
+	}
+
+	nav a {
+		margin: 0 15px;
+	}
+
+	div img {
+		margin-top: 10px;
+	}
+</style>
