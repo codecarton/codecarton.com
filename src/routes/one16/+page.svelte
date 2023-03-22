1f39b6fb9364260b2c0a18dc6c2cb7619db5d71a
Jordan Calhoun <jordancalhoun@gmail.com>
2023-03-22 13:05:21 -0400

    Update screenshot add flex

--- src/routes/one16/+page.svelte
+++ src/routes/one16/+page.svelte
@@ -3,20 +3,21 @@
 	import Headline from '$lib/components/Headline.svelte';
 </script>
 
-<header class="md:text-center">
-	<img src="images/app-icon.svg" alt="One16 App Icon" />
-	<Headline title="One16" tagline="A fraction calculator designed for woodworkers and makers." />
+<div class="flex">
+	<div class="feature">
+		<img src="images/screenshots/initial-screen.png" alt="" class="max-w-sm" />
+		<h2>Fractions, now in first class</h2>
+		<h3>
+			Create equations with fractions or decimals. One16 updates everything to fractions
+			automatically.
+		</h3>
+	</div>
+	<header class="md:text-center">
+		<img src="images/app-icon.svg" alt="One16 App Icon" />
+		<Headline title="One16" tagline="A fraction calculator designed for woodworkers and makers." />
 
-	<DownloadButton />
-</header>
-
-<div class="feature">
-	<img src="images/Intro-iPhone.png" alt="" />
-	<h2>Fractions, now in first class</h2>
-	<h3>
-		Create equations with fractions or decimals. One16 updates everything to fractions
-		automatically.
-	</h3>
+		<DownloadButton />
+	</header>
 </div>
 
 <style lang="postcss">
