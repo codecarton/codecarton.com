79fedb0ee156df4bb3940d6e5f747ffba6dc7fe8
Jordan Calhoun <jordancalhoun@gmail.com>
2023-03-23 12:44:36 -0400

    Privacy layout fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import '../app.css';
+	import { page } from '$app/stores';
 </script>
 
 <svelte:head>
@@ -7,13 +8,14 @@
 </svelte:head>
 
 <slot />
-
-<style lang="postcss">
-	:global(html) {
-		background-color: theme(colors.neutral.900);
-		background-image: url('/images/tape-lines.svg');
-		background-repeat: no-repeat;
-		background-position: top center;
-		padding-top: 150px;
-	}
-</style>
+{#if $page.url.pathname != '/one16/privacy'}
+	<style lang="postcss">
+		:global(html) {
+			background-color: theme(colors.neutral.900);
+			background-image: url('/images/tape-lines.svg');
+			background-repeat: no-repeat;
+			background-position: top center;
+			padding-top: 150px;
+		}
+	</style>
+{/if}
